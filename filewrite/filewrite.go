c550fc9bfaccc9a5da110ae76c33e8353da6a0f0
matsuda13 <genkokurei3@gmail.com>
2022-01-02 13:11:39 +0900

    add buffersize_list

--- filewrite/filewrite.go
+++ filewrite/filewrite.go
@@ -2,8 +2,41 @@ package filewrite
 
 import (
 	"fmt"
+	"time"
+	"os"
+	"bufio"
 )
 
-func Fwrite() {
-	fmt.Println("File Writeします")
+
+func FWrite(buffersize int, filepath string, filesize int) int {
+	var t int
+	n_loop := 20
+	total := 0
+	for i:=0;i<n_loop;i++{
+		//10回繰り返して平均をとる
+	    start := time.Now()
+    	f, err := os.Create(filepath)
+	    if err!=nil{
+    		panic(err)
+	    }
+    	defer f.Close()
+	    buf := bufio.NewWriterSize(f, buffersize)
+    	if buffersize == 0{//no buffering
+	    	for i:=0;i<filesize;i++{
+		    	f.Write([]byte("0"))
+    		}
+			t = int(time.Since(start).Microseconds())
+	    	fmt.Printf("経過:%vμs\n", t)
+    	} else {
+			for i := 0; i <filesize; i++ {
+		        buf.Write([]byte("0"))
+        	}
+	        buf.Flush()
+			t = int(time.Since(start).Microseconds())
+	        fmt.Printf("経過:%vμs\n", t)
+		}
+		total += t;
+	}
+	ave := total/n_loop
+	return ave
 }
--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"os2-1/FileWrite"
+	"os"
+	"strconv"
+	"math"
 )
 
 func main() {
-	now := time.Now()
-	time.Sleep(time.Second*3)
-	filewrite.Fwrite()
-	fmt.Printf("経過:%vms\n", time.Since(now).Milliseconds())
+	var list_buffersize [10] int
+	var str string
+	out_file := "./out/graphdata" + os.Args[2]
+	for i:=0;i<10;i++{
+		list_buffersize[i] = int(math.Pow(2,float64(i+2)))
+	}
+	fmt.Println(os.Args[1], os.Args[2])
+	f, _ := os.Create(out_file)
+	for i:=0;i<len(list_buffersize);i++{
+		buffersize := list_buffersize[i]
+		filesize,_ := strconv.Atoi(os.Args[2]) 
+	    //num, _ := strconv.Atoi(os.Args[2])
+	    average := filewrite.FWrite(buffersize, os.Args[1], filesize)
+	    fmt.Printf("buffersize:%v の処理速度の平均秒数(μs)は、%vμs\n",buffersize,average)
+		str = strconv.Itoa(buffersize) + " " + strconv.Itoa(average) + "\n"
+		buf := []byte(str)
+		f.Write(buf)
+	}
 }
