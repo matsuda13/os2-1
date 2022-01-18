528740ebb5e6535187d01c1404d601bf245bbd5c
matsuda13 <genkokurei3@gmail.com>
2022-01-18 12:29:44 +0900

    add comments

--- FileWrite/filewrite.go
+++ FileWrite/filewrite.go
@@ -1,18 +1,26 @@
 package filewrite
 
 import (
-	//"fmt"
+	"fmt"
 	"time"
 	"os"
 	"bufio"
 	"log"
 )
 
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message
+}
 
+//write "0" to file
 func FWrite(buffersize int, filepath string, filesize int) int {
 	var t int
 	n_loop := 20
 	total := 0
+	//loop 20 times and calculate average time
 	for i:=0;i<n_loop;i++{
 	    start := time.Now()
     	f, err := os.Create(filepath)
@@ -21,19 +29,21 @@ func FWrite(buffersize int, filepath string, filesize int) int {
 	    }
     	defer f.Close()
 	    buf := bufio.NewWriterSize(f, buffersize)
-    	if buffersize == 0{//no buffering
+		//no buffering
+		if buffersize == 0{
 	    	for i:=0;i<filesize;i++{
 		    	f.Write([]byte("0"))
     		}
 			t = int(time.Since(start).Microseconds())
-	    	//fmt.Printf("経過:%vμs\n", t)
+	    	//fmt.Printf("progress:%vμs\n", t)
+		//buffering
     	} else {
 			for i := 0; i <filesize; i++ {
 		        buf.Write([]byte("0"))
         	}
 	        buf.Flush()
 			t = int(time.Since(start).Microseconds())
-	        //fmt.Printf("経過:%vμs\n", t)
+	        //fmt.Printf("progress:%vμs\n", t)
 		}
 		total += t;
 	}
